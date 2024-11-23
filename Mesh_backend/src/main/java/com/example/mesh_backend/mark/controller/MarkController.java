e5ae88689969b45e5005022bb7feff28f89cdec6
YeahOut <pt840072@gmail.com>
2024-11-24 00:16:46 +0900

    feat: 북마크 된 목록만 조회 API

--- Mesh_backend/src/main/java/com/example/mesh_backend/mark/controller/MarkController.java
+++ Mesh_backend/src/main/java/com/example/mesh_backend/mark/controller/MarkController.java
@@ -3,10 +3,13 @@ package com.example.mesh_backend.mark.controller;
 import com.example.mesh_backend.login.entity.User;
 import com.example.mesh_backend.login.service.TokenService;
 import com.example.mesh_backend.mark.service.MarkService;
+import com.example.mesh_backend.mypage.dto.response.ProjectResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/marks")
@@ -19,6 +22,16 @@ public class MarkController {
     public MarkController(MarkService markService, TokenService tokenService) {
         this.markService = markService;
         this.tokenService = tokenService;
+    }
+    @GetMapping
+    @Operation(summary = "북마크된 프로젝트 조회 API", description = "사용자의 북마크된 프로젝트 목록을 반환합니다.")
+    public ResponseEntity<List<ProjectResponseDTO>> getBookmarkedProjects(
+            @RequestHeader("Authorization") String token
+    ) {
+        // 토큰에서 사용자 정보 추출
+        User user = tokenService.getUserFromAccessToken(token.replace("Bearer ", ""));
+        List<ProjectResponseDTO> bookmarkedProjects = markService.getBookmarkedProjects(user);
+        return ResponseEntity.ok(bookmarkedProjects);
     }
 
     @PostMapping("/{project_id}")
